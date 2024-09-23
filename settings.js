5fa6ec224868e5f6fa311733226b928fb4492daf
SATO-SAN-TCH <enygmamax73@gmail.com>
2024-09-23 08:24:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ global.location = "India, West Bengal, Darjeeling" //ur location
 
 //new
 global.botname = 'Zenitsu-Bot8' //ur bot name
-global.ownernumber = '917029257330' //ur owner number
+global.ownernumber = '242069073055' //ur owner number
 global.ownername = '🦄Dream Guy Deepak' //ur owner name
 global.websitex = "https://youtube.com/@dreamguydeepak"
 global.wagc = "https://whatsapp.com/channel/0029VaDEDUaC6ZvewFKv6835"
@@ -19,9 +19,9 @@ global.wm = "Deepak Bot Inc."
 global.botscript = 'https://github.com/DGDEEPAK/Zenitsu-Bot8' //script link
 global.packname = "Zenitsu-Bot"
 global.author = "🦄Dream Guy Deepak"
-global.creator = "917029257330@s.whatsapp.net"
+global.creator = "242069073055@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["917029257330","918348225320"] // Premium User
+global.premium = ["242069073055","242069073055"] // Premium User
 global.hituet = 0
 
 //bot sett
